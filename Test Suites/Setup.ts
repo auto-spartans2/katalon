de51bf6f95bb52d02e3506bb51b82354fec9db85
mooren2 <mooren2@IP-C0A88286.reacts2.aws>
2019-04-18 18:02:14 +0000

    First commit from Katalon Studio to GitHub.

---
+++ Test Suites/Setup.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Setup</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0e9ec5ea-1f97-4a78-981b-53f1d04c5ceb</testSuiteGuid>
+   <testCaseLink>
+      <guid>3eed08cc-673f-4364-a26d-fc1ed0965e2e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Setup</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
